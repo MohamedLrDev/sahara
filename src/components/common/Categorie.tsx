834b8e55fa19be6700f3fa28696b98317aff64ae
MohamedLrDev <lrhali.dev@gmail.com>
2024-10-21 23:04:25 +0100

    categories section done

---
+++ src/components/common/Categorie.tsx
@@ -0,0 +1,26 @@
+interface VehicleCardProps {
+    title: string;
+    image: string;
+  }
+  
+  const Categorie = ({ image, title }: VehicleCardProps) => {
+    return (
+      <div className="container flex justify-center items-center">
+        <div className="relative w-[18rem] h-[12rem] sm:w-[20rem] sm:h-[15rem] lg:w-[24rem] lg:h-[18rem] overflow-hidden rounded-lg group">
+          <img
+            src={image}
+            alt={title}
+            className="w-full h-full object-cover transition-transform duration-300 ease-in-out group-hover:scale-110"
+          />
+          <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center">
+            <h1 className="text-white text-center text-base sm:text-lg md:text-xl lg:text-2xl font-bold">
+              {title}
+            </h1>
+          </div>
+        </div>
+      </div>
+    );
+  };
+  
+  export default Categorie;
+  
