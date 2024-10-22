8727fd6cbf9859e41a4ef51949d5c4b04f4f1d3a
MohamedLrDev <lrhali.dev@gmail.com>
2024-10-22 13:31:06 +0100

    bg image fixing

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,12 +2,16 @@ import Hero from "@/components/layout/Hero";
 import Navbar from "../components/layout/Navbar";
 import ContentContainer from "@/components/layout/ContentContainer";
 import Categories from "@/components/layout/Categories";
+import bg_image from '../assets/images/pexels-photo-4869960.jpeg';
 
 const Home = () => {
   return (
     <ContentContainer>
       {/* Set relative positioning to make sure absolute children are contained */}
-      <div className="relative bg-[url('/D:/Projects/sahara-quad/src/assets/images/pexels-photo-4869960.jpeg')] bg-cover bg-center min-h-screen">
+      <div
+        className="relative bg-cover bg-center min-h-screen"
+        style={{ backgroundImage: `url(${bg_image})` }}
+      >
         {/* The black overlay */}
         <div className="absolute inset-0 bg-black opacity-25"></div>
 
